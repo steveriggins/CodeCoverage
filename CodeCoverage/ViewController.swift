b2f9ddc899e0d9598db5d10944197d2cd43019dd
steveriggins <steve@steveriggins.net>
2015-05-15 21:43:12 -0700

    Initial Commit

---
+++ CodeCoverage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodeCoverage
+//
+//  Created by Steven W. Riggins on 5/15/15.
+//  Copyright (c) 2015 Steve Riggins. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
